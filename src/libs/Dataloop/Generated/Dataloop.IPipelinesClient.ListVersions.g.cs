a3909576c275c4014329b07e04d76363a7feec27
Konstantin S. <havendv@gmail.com>
2026-05-15 18:22:13 +0400

    feat: Updated OpenAPI spec (#114)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/libs/Dataloop/Generated/Dataloop.IPipelinesClient.ListVersions.g.cs
+++ src/libs/Dataloop/Generated/Dataloop.IPipelinesClient.ListVersions.g.cs
@@ -15,7 +15,7 @@ namespace Dataloop
         /// <param name="requestOptions">Per-request overrides such as headers, query parameters, timeout, retries, and response buffering.</param>
         /// <param name="cancellationToken">The token to cancel the operation with</param>
         /// <exception cref="global::Dataloop.ApiException"></exception>
-        global::System.Threading.Tasks.Task<global::Dataloop.CursorPagePipelineVersionListItem> ListVersionsAsync(
+        global::System.Threading.Tasks.Task<global::Dataloop.CursorPageAPIPipelineVersion> ListVersionsAsync(
             string pipelineId,
             double? page = default,
             double? pageSize = default,
@@ -32,7 +32,7 @@ namespace Dataloop
         /// <param name="requestOptions">Per-request overrides such as headers, query parameters, timeout, retries, and response buffering.</param>
         /// <param name="cancellationToken">The token to cancel the operation with</param>
         /// <exception cref="global::Dataloop.ApiException"></exception>
-        global::System.Threading.Tasks.Task<global::Dataloop.AutoSDKHttpResponse<global::Dataloop.CursorPagePipelineVersionListItem>> ListVersionsAsResponseAsync(
+        global::System.Threading.Tasks.Task<global::Dataloop.AutoSDKHttpResponse<global::Dataloop.CursorPageAPIPipelineVersion>> ListVersionsAsResponseAsync(
             string pipelineId,
             double? page = default,
             double? pageSize = default,
